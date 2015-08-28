da84dfd3176fb2fd777cc687edef90e3bcc4df78
David M. Anderson <dma@epsilondelta.us>
2015-08-28 06:50:36 -0700

    Removed demo functions for echoing request and cookie data
    Never used them

--- web.js
+++ web.js
@@ -11,8 +11,6 @@ app.use( favicon( __dirname + '/public/favicon.ico' ) );
 app.use( logfmt.requestLogger() );
 app.use( compression() );
 app.use( express.static( __dirname + '/public' ) );
-app.use( getRequestData );
-app.use( echoRequestData );
 
 var port = Number( process.env.PORT || 6201 );
 app.listen( port,
@@ -20,29 +18,3 @@ app.listen( port,
             {
                 console.log( "Listening on port " + port );
             } );
-
-
-// These functions gather and echo data posted from the client (if any).
-// That is good for experimenting with forms, AJAX, etc.
-// For more useful form handling, particularly parsing of field values,
-// use something like https://www.npmjs.org/package/body
-// or https://github.com/expressjs/body-parser .
-
-function getRequestData( req, res, next )
-{
-    req.setEncoding( 'utf8' );
-    req.data = '';
-    req.on( 'data',
-            function( chunk )
-            {
-                req.data += chunk;
-            } );
-    req.on( 'end', next );
-}
-
-function echoRequestData( req, res, next )
-{
-    res.setHeader( 'content-type', 'text/plain' );
-    res.end( req.data );
-    console.log( 'Request data: ' + req.data );
-}
